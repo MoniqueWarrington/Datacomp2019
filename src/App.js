705d6ea1a77a53a191be10083a94c71617bc8b7b
Monique Warrington <niquewarrington@gmail.com>
2019-06-02 19:55:07 +1200

    Imgs loading, Aidan card added to card group, Button hyperlinks implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Header from './components/Header/Header'
 import './App.css';
-import MoniqueCard from './components/Card/moniquecard';
-//import "react-bootstrap/dist/react-bootstrap.min.js";
+import Cards from './components/Card/Cards';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MoniqueCard />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Card/Cards.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup'
+import MoniqueCardImg from '../../static/img/MoniqueCardImg.jpg';
+import AidanCard from '../../static/img/AidanCard.jpg';
+
+function Cards() {
+  return (
+    <CardGroup>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={MoniqueCardImg} />
+        <Card.Body>
+          <Card.Title>Monique Warrington</Card.Title>
+          <Card.Text>
+            Some really cool information about me!!
+          </Card.Text>
+          <Button variant="primary" href="https://www.linkedin.com/in/monique-warrington/">LinkedIn</Button>
+          <Button variant="primary" href="https://github.com/MoniqueWarrington">GitHub</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={AidanCard} />
+        <Card.Body>
+          <Card.Title>Aidan Harris</Card.Title>
+          <Card.Text>
+            Some really cool information about Aidan!!
+          </Card.Text>
+          <Button variant="primary" href="https://www.linkedin.com/in/aidanharris/">LinkedIn</Button>
+          <Button variant="primary" href="https://github.com/AidanTheDeveloper">GitHub</Button>
+        </Card.Body>
+      </Card>
+    </CardGroup>
+  );
+}
+export default Cards
