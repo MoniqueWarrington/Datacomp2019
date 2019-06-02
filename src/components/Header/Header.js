e74531c0adb675d720a240aae3bd026e3e1a9bc4
AidanTheDeveloper <aharris1001@gmail.com>
2019-06-02 20:31:00 +1200

    Changed Header subtitle, added fading in to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+import Fade from 'react-bootstrap/Fade'
 
-function Header() {
-    return (
+class Header extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            fadeTitleIn: false,
+            fadeSubtitleIn: false
+        };
+        this.onTitleFadeIn = this.onTitleFadeIn.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({fadeTitleIn: true})
+    }
+
+    onTitleFadeIn() {
+        this.setState({fadeSubtitleIn: true})
+    }
+    fadeInStyle = {
+        'transition-duration' : '.5s'
+    }
+    render() {
+        return (
         <Jumbotron>
-            <h1>Aidan Harris & Monique Warrington's Datacomp 2019 Submission</h1>
+            <Fade in={this.state.fadeTitleIn} style={this.fadeInStyle} onEntered={this.onTitleFadeIn}>
+                <h1>Aidan Harris & Monique Warrington</h1>
+            </Fade>
+            <Fade in={this.state.fadeSubtitleIn} style={this.fadeInStyle} >
+                <div id="example-fade-text">
+                    Two software developers trying to make a difference!
+                </div>
+            </Fade>
         </Jumbotron>
-    );
+        )
+    }
 }
 
 export default Header
