e9aa47a21c1fefd7fc2347dd4d30c5536cfa6dde
AidanTheDeveloper <aharris1001@gmail.com>
2019-06-02 18:44:35 +1200

    Replaced bootstrap dependency with react-bootstrap, Added basic header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 
 function Header() {
     return (
-        
+        <Jumbotron>
+            <h1>Aidan Harris & Monique Warrington's Datacomp 2019 Submission</h1>
+        </Jumbotron>
     );
 }
 
