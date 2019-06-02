18fe003ed2468b937e8219dcd03c6fa7e29dd478
Monique Warrington <niquewarrington@gmail.com>
2019-06-02 20:41:27 +1200

    Formatting placing for cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import Header from './components/Header/Header'
 import './App.css';
 import Cards from './components/Card/Cards';
-
-import MoniqueCard from './components/Card/moniquecard';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Cards />
+      <Container className="Cards Container">
+        <Row>
+          <Col></Col>
+          <Col xs={6}>
+            <Cards />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
